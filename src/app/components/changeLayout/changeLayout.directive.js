a9596157482b2a9f37488481a6245e7c080201d8
jianguowen <wen.jian@cardinfolink.com>
2016-06-29 11:40:35 +0800

    refactor:删除无效代码;

--- src/app/components/changeLayout/changeLayout.directive.js
+++ src/app/components/changeLayout/changeLayout.directive.js
@@ -11,14 +11,12 @@ export function ChangeLayoutDirective() {
   return directive;
 
   function linkFunc(scope, element) {
-    console.log('changeLayout is ' + scope.changeLayout)
     //Default State
     if (scope.changeLayout === '1') {
       element.prop('checked', true);
     }
     //Change State
     element.on('change', function() {
-      console.log('element.value is ' + element.is(':checked'))
       if (element.is(':checked')) {
         localStorage.setItem('ma-layout-status', 1);
         scope.$apply(function() {
